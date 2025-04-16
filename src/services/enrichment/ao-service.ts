0662872fa43abda655d6364aac628af464c3f349
Amalendu Pandey <amalendupandey56@gmail.com>
2025-04-16 14:40:57 +0530

    new ao api added

--- src/services/enrichment/ao-service.ts
+++ src/services/enrichment/ao-service.ts
@@ -51,22 +51,52 @@ export class AoEnrichmentService implements IEnrichmentService {
         remarks: "Cannot enrich without token",
         };
     }
+     const [origin, destination] = flightSector.split("-");
+     if (!origin || !destination) {
+       return {
+         sameFlightFare: 0,
+         lowestFlightFare: 0,
+         averageFare: 0,
+         availableStock: 0,
+         errorMessage: "Invalid sector format",
+         remarks: "Expected format: ORIGIN-DEST",
+       };
+     }
     const payload = {
-      Sectors: flightSector,
-      FlightDate:travelDateTime.toLocaleDateString("en-US",{
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit',
-          }).split('-').reverse().join('/'),
-      FlightNumber: "",
+      AgentInfo: {
+        AgentId: "AQAG051265",
+        UserName: "9710101010",
+        AppType: "API",
+        Version: "V1.0",
+      },
+      TripType: "O",
+      AirlineID: "",
+      AvailInfo: [
+        {
+          DepartureStation: origin,
+          ArrivalStation: destination,
+          FlightDate: travelDateTime,
+          FarecabinOption: "E",
+          FareType: "N",
+          OnlyDirectFlight: false,
+        },
+      ],
+      PassengersInfo: {
+        AdultCount: "1",
+        ChildCount: "0",
+        InfantCount: "0",
+      },
     };
+
+    const config = { headers: { TOKEN: token } };
 
     try {
       const res = await axios.post(
-        "http://13.203.38.118:8080/B2BFareTest/Availability",
-        payload
+        "https://airiqapi.tesepr.com/TravelAPI.svc/Availability",
+        payload,
+        config
       );
-      const list = res.data?.Flights?.[0]?.JourneysList?.[0]?.LIST;
+      const items = res.data?.ItineraryFlightList?.[0]?.Items || [];;
 
       let sameFlightFare: number | undefined;
       let lowestFlightFare: number | undefined;
@@ -74,26 +104,28 @@ export class AoEnrichmentService implements IEnrichmentService {
       let totalFareCount = 0;
       let totalSeats = 0;
 
-      for (const item of list) {
-        const aoFlightNo = item.AirlinetList[0]?.AirlineNo?.split(" ").pop()?.trim();
-        const availSeats = item.AvailSeat;
+      for (const item of items) {
+        const flightNo = item.FlightDetails?.[0]?.FlightNumber.split(" ").pop();
+        const availSeats = parseInt(item.FlightDetails?.[0]?.AvailSeat || "0");
 
-        for (const price of item.PriceDetails) {
-          const grossAmount = parseFloat(
-            price.Pricedescription?.[0]?.GrossAmount || "0"
+        for (const fare of item.Fares || []) {
+          const netAmount = parseFloat(
+            fare.Faredescription?.[0]?.NetAmount || "0"
           );
-          if (aoFlightNo && parseInt(aoFlightNo) === flightNumber) {
-            sameFlightFare ??= grossAmount;
-          }
-          if (!lowestFlightFare || grossAmount < lowestFlightFare) {
-            lowestFlightFare = grossAmount;
+
+          if (parseInt(flightNo) === flightNumber) {
+            sameFlightFare ??= netAmount;
           }
 
-          totalFareSum += grossAmount;
+          if (!lowestFlightFare || netAmount < lowestFlightFare) {
+            lowestFlightFare = netAmount;
+          }
+
+          totalFareSum += netAmount;
           totalFareCount++;
         }
 
-        totalSeats = parseInt(availSeats);
+        totalSeats = availSeats;
       }
       const averageFare = totalFareCount > 0 ? totalFareSum / totalFareCount : 0;
       return {
