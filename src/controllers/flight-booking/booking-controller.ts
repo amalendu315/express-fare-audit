49c8f22c443fbf1c4c6b9af288b97788f7c234ea
Amalendu Pandey <amalendupandey56@gmail.com>
2025-06-18 11:17:36 +0530

    chikmanglur

--- src/controllers/flight-booking/booking-controller.ts
+++ src/controllers/flight-booking/booking-controller.ts
@@ -21,7 +21,7 @@
               }
             );
             if (!bookingRes.ok) {
-              throw new Error(`Booking request failed with status ${bookingRes.status}`);
+              throw new Error(`Booking request failed with status ${bookingRes.json()}`);
             }
             const data = await bookingRes.json();
             console.log("Booking response:", data);
